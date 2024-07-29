b0cc7e3b76b4c290fa9d04e4aae08a0b087d624b
Wraami <83980439+Wraami@users.noreply.github.com>
2024-07-29 20:11:25 +0100

    test: add failing test for fizz logic

--- TDDFizzBuzz.Tests/FizzBuzzTests.cs
+++ TDDFizzBuzz.Tests/FizzBuzzTests.cs
@@ -2,10 +2,19 @@ namespace TDDFizzBuzz.Tests
 {
     public class FizzBuzzTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void get_output_should_return_fizz(int input)
         {
+            //Arrange
+            var fizzBuzz = new FizzBuzz();
 
+            //Act
+           string result = fizzBuzz.GetOutput(input);
+
+            //Assert
+            Assert.Equal("Fizz", result);
         }
     }
 }
---
+++ TDDFizzBuzz/FizzBuzz.cs
@@ -0,0 +1,16 @@
+﻿public class FizzBuzz
+{
+    public static void Main(string[] args)
+    {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        int input = 0;
+
+        fizzBuzz.GetOutput(input);
+    }
+
+    public string GetOutput(int number)
+    {
+        return number.ToString();
+    }
+}
