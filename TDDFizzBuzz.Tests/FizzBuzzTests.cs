f87993e224388ef1fb41e287721bafc535980588
Wraami <83980439+Wraami@users.noreply.github.com>
2024-07-29 21:05:27 +0100

    test: add additional test cases for fizz and buzz parsing

--- TDDFizzBuzz.Tests/FizzBuzzTests.cs
+++ TDDFizzBuzz.Tests/FizzBuzzTests.cs
@@ -18,7 +18,7 @@ namespace TDDFizzBuzz.Tests
         [InlineData(3)]
         [InlineData(6)]
         [InlineData(9)]
-        public void get_output_should_return_fizz(int input)
+        public void FizzParser_ShouldReturnFizz_WhenDivisibleByThreee(int input)
         {
             //Arrange
 
@@ -29,10 +29,27 @@ namespace TDDFizzBuzz.Tests
             Assert.Equal("Fizz", result);
         }
 
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(4)]
+        [InlineData(44)]
+        [InlineData(71)]
+        public void FizzParser_ShouldReturnFizz_WhenNotDivisibleByThreee(int input)
+        {
+            // Arrange
+         
+            // Act
+            string result = _fizzparser.ParseUserInput(input);
+            
+            // Assert
+            Assert.NotEqual("Fizz", result);
+        }
+
         [Theory]
         [InlineData(5)]
         [InlineData(10)]
-        public void get_output_should_return_buzz(int input)
+        public void BuzzParser_ShouldReturnBuzz_WhenDivisibleByFive(int input)
         {
 
             //Act
@@ -41,6 +58,20 @@ namespace TDDFizzBuzz.Tests
             //Assert
             Assert.Equal("Buzz", result);
 
+        }
+
+        [Theory]
+        [InlineData(14)]
+        [InlineData(17)]
+        [InlineData(87)]
+        [InlineData(407)]
+        public void BuzzParser_ShouldNotReturnBuzz_WhenNotDivisibleBy5(int input)
+        {
+            // Arrange
+            // Act
+            string result = _buzzparser.ParseUserInput(input);
+            // Assert
+            Assert.NotEqual("Buzz", result);
         }
     }
 }
